93eededd1fed90a05c95580cc451d28976980a71
Feng Ya <fengya04@126.com>
2017-03-11 18:01:59 +0800

    download csv

--- index.js
+++ index.js
@@ -30,7 +30,11 @@ $(document).ready(() => {
         })
       }
     }
-    console.log(array)
+
+    const csv = new csvWriter()
+		const data = encodeURIComponent(csv.arrayToCSV(array))
+		const encodedUri = `data:text/csv;charset=utf-8,${data}`
+		window.open(encodedUri)
   }
 
   $('button:contains("Save")').remove()
@@ -51,3 +55,61 @@ $(document).ready(() => {
     $(gp).append(button)
   })
 })
+
+/**
+ * Class for creating csv strings
+ * Handles multiple data types
+ * Objects are cast to Strings
+ **/
+
+function csvWriter(del, enc) {
+    this.del = del || ','; // CSV Delimiter
+    this.enc = enc || '"'; // CSV Enclosure
+
+    // Convert Object to CSV column
+    this.escapeCol = function (col) {
+        if(isNaN(col)) {
+            // is not boolean or numeric
+            if (!col) {
+                // is null or undefined
+                col = '';
+            } else {
+                // is string or object
+                col = String(col);
+                if (col.length > 0) {
+                    // use regex to test for del, enc, \r or \n
+                    // if(new RegExp( '[' + this.del + this.enc + '\r\n]' ).test(col)) {
+
+                    // escape inline enclosure
+                    col = col.split( this.enc ).join( this.enc + this.enc );
+
+                    // wrap with enclosure
+                    col = this.enc + col + this.enc;
+                }
+            }
+        }
+        return col;
+    };
+
+    // Convert an Array of columns into an escaped CSV row
+    this.arrayToRow = function (arr) {
+        var arr2 = arr.slice(0);
+
+        var i, ii = arr2.length;
+        for(i = 0; i < ii; i++) {
+            arr2[i] = this.escapeCol(arr2[i]);
+        }
+        return arr2.join(this.del);
+    };
+
+    // Convert a two-dimensional Array into an escaped multi-row CSV
+    this.arrayToCSV = function (arr) {
+        var arr2 = arr.slice(0);
+
+        var i, ii = arr2.length;
+        for(i = 0; i < ii; i++) {
+            arr2[i] = this.arrayToRow(arr2[i]);
+        }
+        return arr2.join("\r\n");
+    };
+}
