76347d363c256a3abbe8c2944c39027d343a788d
Feng Ya <fengya04@126.com>
2017-03-11 15:23:55 +0800

    Add button

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
   $('li.example-gp').each((index, gp) => {
+    $('button', gp).remove()
+
     const children = $(gp).clone().children().toArray()
     const last = $('.example:last', gp).index()
-    const example = $('<div>').append(children.slice(0,last+1))
-    const normal = $('<div>').append(children.slice(last+1))
-    console.log(gp, example, normal)
+    const front = $('<div>').append(children.slice(0,last+1))
+    const back = $('<div>').append(children.slice(last+1))
+    // console.log(gp, front, back)
+
+    const button = $('<button>Add</button>')
+    button.click(() => {
+      console.log(front.html())
+      console.log(back.html())
+    })
+    $(gp).append(button)
   })
 })
