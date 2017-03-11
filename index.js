74a726130319476034eaa4d0665d77860ebe9f58
Feng Ya <fengya04@126.com>
2017-03-11 15:03:20 +0800

    example

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
-  console.log('Hello jQuery.')
+  $('li.example-gp').each((index, gp) => {
+    const children = $(gp).clone().children().toArray()
+    const last = $('.example:last', gp).index()
+    const example = $('<div>').append(children.slice(0,last+1))
+    const normal = $('<div>').append(children.slice(last+1))
+    console.log(gp, example, normal)
+  })
 })
