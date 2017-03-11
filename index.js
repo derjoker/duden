221bd124542bfe7c2548d182277726d8b5fffb6a
Feng Ya <fengya04@126.com>
2017-03-11 17:57:09 +0800

    get

--- index.js
+++ index.js
@@ -19,6 +19,25 @@ $(document).ready(() => {
     localStorage.setItem(key, JSON.stringify(value))
   }
 
+  function get() {
+    let array = []
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)
+      if (key.startsWith(VERSION)) {
+        const value = JSON.parse(localStorage.getItem(key))
+        Object.keys(value).forEach((item) => {
+          array.push([item, value[item]])
+        })
+      }
+    }
+    console.log(array)
+  }
+
+  $('button:contains("Save")').remove()
+
+  const save = $('<button>Save</button>').click(() => get())
+  $('h1#page-title').after(save)
+
   $('li.example-gp').each((index, gp) => {
     $('button', gp).remove()
 
@@ -28,10 +47,7 @@ $(document).ready(() => {
     const back = $('<div>').append(children.slice(last+1))
     // console.log(gp, front, back)
 
-    const button = $('<button>Add</button>')
-    button.click(() => {
-      set(front, back)
-    })
+    const button = $('<button>Add</button>').click(() => set(front, back))
     $(gp).append(button)
   })
 })
